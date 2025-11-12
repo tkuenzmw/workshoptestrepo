635eccc6dc4e60ff262addd381316aceed1c3197
Thomas Künzel <127113401+tkuenzmw@users.noreply.github.com>
2025-11-12 09:46:11 +0000

    changed test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 matrix = np.random.randint(1, 101, size=(int(N), int(N)))
 mean_value = np.mean(matrix)
 
-print("Generated 5x5 Matrix:")
+print(f"Generated {int(N)}x{int(N)} Matrix:")
 print(matrix)
 print(f"Mean of all elements: {mean_value}")
 
